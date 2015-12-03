a0def4e6d1e0004e3f36a5e8c03fabcbb4a35e5c
Walter Kuppens <reshurum@gmail.com>
2015-12-03 15:03:19 -0800

    Added inputs

--- src/js/ginger.js
+++ src/js/ginger.js
@@ -57,20 +57,26 @@ var Ginger = function() {
   };
 
   function load() {
-    loadTextures();
-    loadMeshes();
+    loadTextures(loadMeshes);
   }
 
-  function loadTextures() {
+  function loadTextures(callback) {
     var goal = Object.keys(textures).length;
     var progress = 0;
 
+    // Loads textures asynchronously.
     var load = function(path, texture) {
       textureLoader.load(path, function(loadedTexture) {
         textures[texture].texture = loadedTexture;
         progress++;
 
+        // Once all textures are loaded the callback is called.
+        // This allows chaining mesh loading which requires textures.
         if (progress >= goal) {
+          if (callback != null) {
+            callback();
+          }
+
           return;
         }
       });
@@ -78,13 +84,14 @@ var Ginger = function() {
 
     var textureLoader = new THREE.TextureLoader();
 
+    // Begin the async load.
     for (var texture in textures) {
       var path = textures[texture].path;
       load(path, texture);
     }
   }
 
-  function loadMeshes() {
+  function loadMeshes(callback) {
     var goal = Object.keys(meshes).length;
     var progress = 0;
 
@@ -97,6 +104,7 @@ var Ginger = function() {
       }
     };
 
+    // Loads the meshes asynchronously.
     var load = function(path, mesh) {
       jsonLoader.load(path, function(geometry) {
         var texture;
@@ -107,27 +115,42 @@ var Ginger = function() {
 
         var material = new THREE.MeshLambertMaterial({
           map: texture,
+          morphTargets: true
         });
 
         meshes[mesh].mesh = new THREE.Mesh(geometry, material);
         progress++;
 
-        // Add the meshes to the scene when the goal is met.
+        // Once all meshes are loaded, all meshes are added to the scene.
+        // Optionally a callback is available.
         if (progress >= goal) {
           addMeshes();
+
+          if (callback != null) {
+            callback();
+          }
+
           return;
         }
       });
     };
 
+    // Begin the async load.
     for (var mesh in meshes) {
       var path = meshes[mesh].path;
       load(path, mesh);
     }
   }
 
+  function onresize(event) {
+    recalculateAspect();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
   function recalculateAspect() {
     aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = aspect;
+    camera.updateProjectionMatrix();
   }
 
   function animate() {
@@ -139,25 +162,33 @@ var Ginger = function() {
     init: function() {
       scene = new THREE.Scene();
 
+      // Load ginger in the background.
+      load();
+
       // Find the initial aspect.
-      recalculateAspect();
+      aspect = window.innerWidth / window.innerHeight;
 
       camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 10000);
       camera.position.y = 5;
       camera.position.z = 10;
 
       // Create a renderer the size of the entire window.
-      renderer = new THREE.WebGLRenderer();
+      renderer = new THREE.WebGLRenderer({
+        antialias: true
+      });
       renderer.setSize(window.innerWidth, window.innerHeight);
 
-      document.body.appendChild(renderer.domElement);
+      // Add the canvas to the renderer wrapper so the panel
+      // stays above the canvas.
+      document.getElementById('renderer').appendChild(renderer.domElement);
 
+      window.onresize = onresize;
+
+      // Let there be light! The light is simply a directional light that
+      // shines directly inter Ginger's face.
       var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
       directionalLight.position.set(0, 0, 1);
       scene.add(directionalLight);
-
-      // Load ginger in the background.
-      load();
 
       // Start the render loop.
       animate();
