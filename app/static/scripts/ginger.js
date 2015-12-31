843c0d7714d06b61c71ac355f9ec4cc956c2fa3b
Justin Ribeiro <justin@justinribeiro.com>
2015-12-31 12:44:30 -0800

    Update GAE config, add GA, add offline GA, remove input, change theme color

--- app/static/scripts/ginger.js
+++ app/static/scripts/ginger.js
@@ -843,6 +843,15 @@ function initClipboard() {
   });
 }
 
+// GA platinum-sw-offline-analytics handles offline
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-20667086-9', 'auto');
+ga('send', 'pageview');
+
 function appInit() {
   var version = '1';
 
